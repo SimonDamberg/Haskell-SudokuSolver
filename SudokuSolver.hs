83d621a615bb51edd3a47412b05b00442797181f
SimonXPS <damberg.simon00@gmail.com>
2020-02-12 14:33:31 +0100

    Update SudokuSolver.hs

--- SudokuSolver.hs
+++ SudokuSolver.hs
@@ -1,3 +1,6 @@
+import Data.List
+import Data.Traversable
+
 data Cell = Fixed Int | Possible [Int] deriving (Show, Eq)
 
 type Row = [Cell]
@@ -35,12 +38,5 @@ Removes val from every other cell in the 3x3 square corresponding to val
 -}
 checkSquare :: Board -> Cell -> Board
 checkSquare board val = undefined
-
-{- checkColumn board val
-Removes val from every other cell in the column corresponding to val
-
--}
-checkColumn :: Board -> Cell -> Board
-checkColumn board val = undefined
 
 
