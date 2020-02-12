cc20a058f95b7b2233699bffd83b7bef37a9e4ab
SimonXPS <damberg.simon00@gmail.com>
2020-02-12 14:37:05 +0100

    Update SudokuSolver.hs

--- SudokuSolver.hs
+++ SudokuSolver.hs
@@ -7,17 +7,16 @@ type Row = [Cell]
 
 type Board = [Row]
 
-{- makeBoard string
-Creates a board with all possible values in cells from a string
-
--}
-makeBoard :: String -> Board
-makeBoard string = undefined
-
 {- finishedBoard board
 Checks if all cells are filled with one value-}
 finishedBoard :: Board -> Bool
 finishedBoard board = undefined
+
+{- makeBoard string
+Creates a board with all possible values in cells from a string
+-}
+makeBoard :: String -> Board
+makeBoard string = undefined
 
 {- displayBoard board
 Converts the board into graphical rows
@@ -27,14 +26,12 @@ displayBoard board = undefined
 
 {- checkRow row val
 Removes val from every other cell in row
-
 -}
 checkRow :: Row -> Cell -> Row
 checkRow row val = undefined
 
 {- checkSquare board val
 Removes val from every other cell in the 3x3 square corresponding to val
-
 -}
 checkSquare :: Board -> Cell -> Board
 checkSquare board val = undefined
