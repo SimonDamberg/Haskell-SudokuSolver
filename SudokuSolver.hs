2a1e517fb2cd9a3f172c33409a7b8d316fdaf4ec
Theo Meier Ström <meier.strom.theo@gmail.com>
2020-02-17 21:46:26 +0100

    makeBoar och displayBoard
    makeBoard och displayBoard fungerar nu

--- SudokuSolver.hs
+++ SudokuSolver.hs
@@ -1,5 +1,6 @@
 import Data.List
 import Data.Traversable
+import Data.List.Split
 
 data Cell = Fixed Int | Possible [Int] deriving (Show, Eq)
 
@@ -15,14 +16,25 @@ finishedBoard board = undefined
 {- makeBoard string
 Creates a board with all possible values in cells from a string
 -}
-makeBoard :: String -> Board
-makeBoard string = undefined
+makeBoard :: String -> Maybe Board
+makeBoard string
+  |(length string) /= 81 = Nothing
+  |otherwise = Just (map makeBoard' (chunksOf 9 string))
+  where makeBoard' [] = []
+        makeBoard' (x:xs)
+          |x == '*' = [Possible [1..9]] ++ makeBoard' xs
+          |otherwise = [Fixed (read [x] ::Int)] ++ makeBoard' xs
 
 {- displayBoard board
 Converts the board into graphical rows
 -}
 displayBoard :: Board -> String
-displayBoard board = undefined
+displayBoard board = unlines (map displayBoard' board)
+  where displayBoard' [] = []
+        displayBoard' (x:xs) =
+          case x of
+          Fixed x -> show x ++ " " ++ displayBoard' xs
+          _ -> "* " ++ displayBoard' xs
 
 {- checkRow row val
 Removes val from every other cell in row
